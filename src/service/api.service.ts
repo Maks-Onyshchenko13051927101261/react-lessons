7defa765864840ec04ea825464de256853b0bbbb
Maks Onyshchenko <onyshchenkomaksymm@gmail.com>
2025-06-12 04:43:19 +0300

    fix service hw1

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,6 +1,6 @@
 import type {TodoModI} from "../modules/TodoModI.ts";
 
-const endpoint = import.meta.env.VITE_API_URL;
+const endpoint = import.meta.env.VITE_API_BASE_URL;
 
 const loadTodos = async ():Promise<TodoModI[]> => {
     return await fetch(endpoint + '/todos').then((res) => res.json());
